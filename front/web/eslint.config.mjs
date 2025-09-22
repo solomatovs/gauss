03e75e73149a3bb7caedc82217eb5fc46df7adba
Sergey Solomatov <solomatovs@gmail.com>
2025-09-22 18:37:48 +0000

    fix dev containers

--- front/web/eslint.config.mjs
+++ front/web/eslint.config.mjs
@@ -5,25 +5,33 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{js,jsx}'],
+    files: ["**/*.{js,jsx}"],
     extends: [
       js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
+      reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: 'latest',
+        ecmaVersion: "latest",
         ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+        sourceType: "module",
       },
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
     },
   },
-])
+  {
+    files: ["vite.config.js"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+]);
--- front/web/vite.config.js
+++ front/web/vite.config.js
@@ -1,10 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    open: false
-  },
-})
+export default defineConfig((configEnv) => {
+  const env = loadEnv(configEnv.mode, process.cwd(), "");
+
+  const BASE_URL = env.BASE_URL ?? "";
+  const BASE = BASE_URL === "" ? "/" : BASE_URL;
+  
+  console.log(process.cwd());
+  console.log(configEnv);
+  console.log("BASE_URL: ", BASE_URL);
+  console.log("BASE: ", BASE);
+  
+  return {
+    plugins: [react()],
+    base: BASE,
+    server: {
+      open: false,
+    },
+  };
+});
